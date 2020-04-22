7d709a5ac2f3428f8d5bd5d47dbbe824b7800cdf
Maleda Tessema <mal.bt27@gmail.com>
2020-04-22 17:23:07 -0400

    connect to the Session using sessionmaker

--- phase1/consumer-to-SQL.py
+++ phase1/consumer-to-SQL.py
@@ -5,6 +5,8 @@ import psycopg2
 from sqlalchemy.dialects.postgresql import psycopg2
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, session
 
 Base = declarative_base()
 
@@ -34,10 +36,10 @@ class XactionConsumer:
         # data gets lost!
         # add a way to connect to your database here.
 
-        # self.engine = sqlalchemy.create_engine('mysql+pymysql://root:zipcoder@localhost/kafka')
-        # self.conn = psycopg2.conn("host=localhost dbname=kafka user=postgres")
-        #self.engine = sqlalchemy.create_engine('postgres+psycopg2://root:zipcoder@localhost/kafka')
-        self.conn = create_engine('postgres+psycopg2://root:zipcoder@localhost/kafka')
+        self.engine = create_engine('mysql+pymysql://root:zipcoder@localhost/kafka')
+        session = sessionmaker(bind=self.engine)
+        session = session()
+
         # Go back to the readme.
 
     def handleMessages(self):
@@ -45,6 +47,9 @@ class XactionConsumer:
             message = message.value
             print('{} received'.format(message))
             self.ledger[message['custid']] = message
+            message_sql= transaction(custid=message['custid'], type=message['type'], date=message['date'])
+            session.ass(message_sql)
+            session.commit()
             # add message to the transaction table in your SQL usinf SQLalchemy
             if message['custid'] not in self.custBalances:
                 self.custBalances[message['custid']] = 0
@@ -54,47 +59,6 @@ class XactionConsumer:
                 self.custBalances[message['custid']] -= message['amt']
             print(self.custBalances)
             print()
-            with self.engine.connect() as con:
-                with con.begin():
-                    # cur = conn.cursor()
-                    con.execute("INSERT INTO transaction(custid, type, amt) VALUES (int(message['custid']),"
-                                " str(message['type']), int(message['amt'])")
-# >>> conn.execute(addresses.insert(), [
-# ...    {'user_id': 1, 'email_address' : 'jack@yahoo.com'},
-# ...    {'user_id': 1, 'email_address' : 'jack@msn.com'},
-# ...    {'user_id': 2, 'email_address' : 'www@www.org'},
-# ...    {'user_id': 2, 'email_address' : 'wendy@aol.com'},
-# ... ])
 
-# if __name__ == "__main__":
-#     c = XactionConsumer()
-#     c.handleMessages()
 
-# def create_tbl_consumer_tran():
-#     conn = psycopg2.connect("host=localhost dbname=kafka user=postgres")
-#     cur = conn.cursor()
-#     cur.execute("""
-#         drop table if exists transaction;
-#         CREATE TABLE transaction(
-#                 id = Column(Integer, primary_key=True)
-#                 custid = Column(Integer)
-#                 type = Column(String(250), nullable=False)
-#                 date = Column(Integer)
-#                 amt = Column(Integer)
-#     )
-#     """)
-#     conn.commit()
-#
-#
-# def insert_to_tbl():
-#     with open("how to store the data from a dict format", 'r') as f:
-#         next(f)
-#         cur.copy_from(f, 'transaction', sep=',')
-#         conn.commit()
-def connect_pst():
-    psycopg2.connect("host=localhost dbname=kafka user=postgres")
-    cur = conn.cursor()
-    with open("/Users/mtessema/Desktop/PY/TSLA.csv", 'r') as f:
-        next(f)
-        cur.copy_from(f, 'stock', sep=',')
-        conn.commit()
+
